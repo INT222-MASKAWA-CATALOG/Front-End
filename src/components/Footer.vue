097ef66174288976f4ca177b7f67af09931a1003
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-08-18 22:35:30 +0700

    [ADD] Footer Component

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer object-none object-bottom">
+    <div class="bg-footer h-16 opacity-50 fixed w-full">
+      <router-link to="/team"></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
