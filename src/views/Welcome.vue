0d71ea6c232d299816f931d0c86521db168ebf7a
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-08-18 20:34:11 +0700

    [Done] Welcome Page

---
+++ src/views/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <img src="../assets/maskawa_logo.png" class="w-3/5 mx-auto" />
+  <router-link to="/home">
+    <a class="text-center bg-yellowPastel py-4 px-12 rounded-full">Start ></a>
+  </router-link>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+  },
+};
+</script>
