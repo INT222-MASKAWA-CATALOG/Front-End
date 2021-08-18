b93efad98a9dc8364fac17890904737c0d8e093e
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-08-18 20:37:18 +0700

    Set Environment

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome.vue'
 
 const routes = [
   {
     path: '/',
+    name: 'Welcome',
+    component: Welcome
+  },
+  {
+    path: '/home',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,8 @@ module.exports = {
       indigo: colors.indigo,
       purple: colors.violet,
       pink: colors.pink,
+      yellowPastel: "#F3F094",
+      grayon: "#F5F5F5"
     },
     spacing: {
       px: '1px',
