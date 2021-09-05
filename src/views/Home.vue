0b3d1e3b71a9e8e2813076fe71954bbe62e5d5bb
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-09-05 22:24:41 +0700

    [EDIT] Home with carousel components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
     <nav-bar head="MASKAWA" />
+    <carousel />
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue'
+import Carousel from '../components/Carousel.vue'
 export default {
     components: {
         NavBar,
+        Carousel,
     }
 }
 </script>
