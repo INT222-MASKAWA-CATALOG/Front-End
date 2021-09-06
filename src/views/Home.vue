f9ebe34e3949dc5c5eda830031aaca975a8e4701
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-09-06 19:45:47 +0700

    [UPDATE] Carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <nav-bar head="MASKAWA" />
     <p class="text-gray-300 text-xl font-light my-3">Get Ready for the MASKAWA</p>
-    <carousel class="" />
+    <carousel class="text-3xl mx-8" />
 </template>
 
 <script>
