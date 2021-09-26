b35ac806ab4cd3fd6774054b1e115b278eff72e6
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-09-26 15:38:10 +0700

    [ADD] AllProductCard View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,61 @@
 <template>
-    <nav-bar head="MASKAWA" />
-    <p class="text-gray-300 text-xl font-light my-3">Get Ready for the MASKAWA</p>
-    <carousel class="text-3xl mx-8" />
+  <nav-bar head="MASKAWA" />
+  <p class="text-gray-300 text-xl font-light my-3 text-center">Get Ready for the MASKAWA</p>
+  <carousel class="text-3xl mx-8 text-center" />
+  <div class="grid grid-cols-3 gap-12 my-12 mx-40">
+    <card-product v-for="product in products" :key="product.productid" :p="product" v-on:click="toggleProductModal(product.productid)"/>
+  </div>
+
+  <product-modal v-if="showProductModal" @close-product-modal="closeProductModal" :e="this.eachProduct" />
+  <div v-if="showProductModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
 </template>
 
 <script>
-import NavBar from '../components/NavBar.vue'
-import Carousel from '../components/Carousel.vue'
+import NavBar from "../components/NavBar.vue";
+import Carousel from "../components/Carousel.vue";
+import CardProduct from "../components/CardProduct.vue";
+import ProductModal from "../components/ProductModal.vue";
 export default {
-    components: {
-        NavBar,
-        Carousel,
+  components: {
+    NavBar,
+    Carousel,
+    CardProduct,
+    ProductModal,
+  },
+  props: {},
+  data() {
+    return {
+      productlink: "http://localhost:3000/product",
+      products: [],
+      eachProduct: [],
+      toggleId: null,
+      showProductModal: false,
+    };
+  },
+  methods: {
+    async fetchProduct() {
+      const res = await fetch(this.productlink);
+      const data = await res.json();
+      return data;
+    },
+    async fetchEachProduct() {
+      const res = await fetch(`${this.productlink}/${this.toggleId}`);
+      const data = await res.json();
+      return data;
+    },
+    async toggleProductModal (id) {
+      this.showProductModal = !this.showProductModal;
+      this.toggleId = id;
+      this.eachProduct = await this.fetchEachProduct();
+    },
+    async closeProductModal () {
+      console.log("close")
+      this.showProductModal = !this.showProductModal;
     }
-}
+  },
+  async created() {
+    this.products = await this.fetchProduct();
+    
+  },
+};
 </script>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <img src="../assets/maskawa_logo.png" class="w-3/5 mx-auto" />
-  <router-link to="/home">
-    <a class="text-center bg-yellowPastel py-4 px-12 rounded-full">Start ></a>
+  <router-link to="/home" class="text-center mx-auto">
+    <p class="text-center bg-yellowPastel py-4 px-12 rounded-full w-40 mx-auto">Start ></p>
   </router-link>
 </template>
 
