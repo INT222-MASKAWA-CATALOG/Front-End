7175a12a683c18853fbb9c3e19742a9c00cff460
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-10-14 22:34:10 +0700

    [Start] Manage System Page

---
+++ src/views/ManageSystem.vue
@@ -0,0 +1,29 @@
+<template>
+	<nav-bar head="Manage System"/>
+	
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue';
+
+export default {
+	components: {
+		NavBar
+	},
+	props: {
+
+	},
+	data() {
+	return {
+
+	}
+	},
+	methods: {
+
+	},
+	async create() {
+
+	}
+};
+
+</script>
---
+++ src/views/manage_system_page/ManageColor.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+	components: {
+		NavBar
+	},
+	props: {
+
+	},
+	data() {
+	return {
+
+	}
+	},
+	methods: {
+
+	},
+	async create() {
+
+	}
+};
+
+</script>
