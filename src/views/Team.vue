ac3ffd7d591837f81a68c634d1d7d2ad6bacbeec
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-11-24 21:46:52 +0700

    [Update] Team Page Some Responsive

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,24 +1,28 @@
 <template>
 	<div class="Team">
 		<nav-bar head="Team" />
-		<div class="grid grid-cols-3 gap-20 mt-8 mx-16">
+		<div class="xl:grid xl:grid-cols-3 xl:gap-20 xl:mt-8 xl:mx-16 
+								lg:grid lg:grid-cols-3 lg:gap-10 lg:mt-16 lg:mx-10">
 			<div class="bg-tp text-center">
-				<img src="../assets/mew.png" class="rounded-full mt-8 mb-8 xl:w-72 md:w-72 w-48 mx-auto" style="filter:brightness(90%);">
-				<div class="rounded-xl w-max mx-auto pt-1 mt-8">62130500056</div>
+				<img src="../assets/mew.png" style="filter:brightness(90%);"
+					class="rounded-full mx-auto xl:mt-8 xl:mb-8 xl:w-72 md:w-72 lg:w-48">
+				<div class="xl:w-max xl:mx-auto xl:pt-1 xl:mt-8">62130500056</div>
 				<p>PIYATHIDA THANEEWAN</p>
-				<p class="text-sm mb-16">RESPONSIBILITY: BACK-END & DATABASE & DEVOPS</p>
+				<p class="xl:text-sm xl:mb-16 text-xs">RESPONSIBILITY: BACK-END & DATABASE & DEVOPS</p>
 			</div>
 			<div class="bg-tb text-center">
-				<img src="../assets/sk.jpg" class="rounded-full mt-8 mb-8 xl:w-72 md:w-72 w-48 mx-auto"  style="filter:brightness(90%);">
-				<div class="rounded-xl w-max mx-auto pt-1">62130500060</div>
+				<img src="../assets/sk.jpg" style="filter:brightness(90%);"
+					class="rounded-full mx-auto xl:mt-8 xl:mb-8 xl:w-72 md:w-72 lg:w-48">
+				<div class="xl:w-max xl:mx-auto xl:pt-1 xl:mt-8">62130500060</div>
 				<p>PONGPICHET TANTHANAWIKRAI</p>
-				<p class="text-sm mb-16">RESPONSIBILITY: FRONT-END & BACK-END</p>
+				<p class="xl:text-sm xl:mb-16 text-xs">RESPONSIBILITY: FRONT-END & BACK-END</p>
 			</div>
 			<div class="bg-ty text-center">
-				<img src="../assets/win.jpg" class="rounded-full mt-8 mb-8 xl:w-72 md:w-72 w-48 mx-auto"  style="filter:brightness(120%);">
-				<div class="rounded-xl w-max mx-auto pt-1">62130500064</div>
+				<img src="../assets/win.jpg" style="filter:brightness(170%);"
+					class="rounded-full mx-auto xl:mt-8 xl:mb-8 xl:w-72 md:w-72 lg:w-48">
+				<div class="xl:w-max xl:mx-auto xl:pt-1 xl:mt-8">62130500064</div>
 				<p>POSSAWAT TANAM</p>
-				<p class="text-sm mb-16">RESPONSIBILITY: BACK-END & DATABASE & DEVOPS</p>
+				<p class="xl:text-sm xl:mb-16 text-xs">RESPONSIBILITY: BACK-END & DATABASE & DEVOPS</p>
 			</div>
 		</div>
 	</div>
